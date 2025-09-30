1e6fd8aee6f6c26cd9dae04d204dd4cfc2ee8129
luli <luizamesquita2339@gmail.com>
2025-09-30 15:16:45 -0300

    Update Relatorio.java

--- src/Relatorio.java
+++ src/Relatorio.java
@@ -1,4 +1,16 @@
 public class Relatorio{
-  private Drone drones; // voos realizados
-  private Registro registro; //ultimas medicoes
+    private RegistroPlantacao registro;
+
+    public Relatorio(RegistroPlantacao registro) {
+        this.registro = registro;
+    }
+
+    public void gerarResumo() {
+        System.out.println("=== Relatório da Missão ===");
+        System.out.println("Área: " + registro.getMissao().getArea().getId());
+        System.out.println("Drone: " + registro.getMissao().getDrone().getId());
+        System.out.println("Temperatura: " + registro.getTemperatura());
+        System.out.println("Umidade: " + registro.getUmidade());
+        System.out.println("Pragas: " + (registro.isPragas() ? "True" : "False"));
+    }
 }
