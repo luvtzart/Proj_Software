82981e45331c5dcc0e073ba9deba1631e8b2cfd2
luli <luizamesquita2339@gmail.com>
2025-09-30 15:15:51 -0300

    Update Drone.java

--- src/Drone.java
+++ src/Drone.java
@@ -1,9 +1,34 @@
 public class Drone{
-  private String ID;
-  private bool status;
-  //sensores disponiveis
+    private String id;
+    private boolean sensorDisponivel;
+    private String status;
 
+    public Drone(String id, boolean sensorDisponivel, String status) {
+        if (id == null || id.isBlank()) {
+            throw new IllegalArgumentException("ID do drone não pode ser nulo ou vazio.");
+        }
+        this.id = id;
+        this.sensorDisponivel = sensorDisponivel;
+        this.status = status;
+    }
 
+    public String getId() {
+        return id;
+    }
 
-  
+    public boolean isSensorDisponivel() {
+        return sensorDisponivel;
+    }
+    
+    public String getStatus() {
+        return status;
+    }
+
+    public String setStatus(String status) {
+        return this.status = status;
+    }
+
+    public void setSensorDisponivel(boolean sensorDisponivel) {
+        this.sensorDisponivel = sensorDisponivel;
+    }
 }
