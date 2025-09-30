e3ffceeba7333965c73c2265ade3f69e170d4ef2
luli <luizamesquita2339@gmail.com>
2025-09-30 15:14:49 -0300

    Update Agendamento.java

--- src/Agendamento.java
+++ src/Agendamento.java
@@ -1,5 +1,16 @@
-public class Agendamento{
-  // private data
-  private String area;
-  //private sensores a usar
+public class Relatorio{
+    private RegistroPlantacao registro;
+
+    public Relatorio(RegistroPlantacao registro) {
+        this.registro = registro;
+    }
+
+    public void gerarResumo() {
+        System.out.println("=== Relatório da Missão ===");
+        System.out.println("Área: " + registro.getMissao().getArea().getId());
+        System.out.println("Drone: " + registro.getMissao().getDrone().getId());
+        System.out.println("Temperatura: " + registro.getTemperatura());
+        System.out.println("Umidade: " + registro.getUmidade());
+        System.out.println("Pragas: " + (registro.isPragas() ? "True" : "False"));
+    }
 }
