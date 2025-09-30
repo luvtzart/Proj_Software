b93f368cb4de32dd0aaa9b623b2f7cf16b85aba6
luli <luizamesquita2339@gmail.com>
2025-09-30 15:18:10 -0300

    Update and rename main.java to Main.java

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+public class Main{
+    public static void main(String[] args) {
+        // Criando instâncias de Area e Drone
+        Area area1 = new Area(1000, "A1", "Fazendinha", "Milho");
+        Drone drone1 = new Drone("D1", true, "Disponível");
+
+        // Criando um Agendamento
+        Agendamento agendamento1 = new Agendamento("AG1", "2024-06-01", area1, drone1);
+
+        // Criando um RegistroPlantacao
+        RegistroPlantacao registro1 = new RegistroPlantacao("imagem1.jpg", agendamento1, 25.5, 60.0, false);
+
+        // Criando um Relatorio
+        Relatorio relatorio1 = new Relatorio(registro1);
+
+        // Gerando o resumo do relatório
+        relatorio1.gerarResumo();
+    }
+}
