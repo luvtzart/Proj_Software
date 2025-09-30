8108e7b360b0b0b0ea301dc5cb4369b1a4a4aad4
Allegretti77 <vini16.allegretti@gmail.com>
2025-09-30 17:57:48 -0300

    Alteracoes Relatorio

--- src/Relatorio.java
+++ src/Relatorio.java
@@ -7,10 +7,12 @@ public class Relatorio{
 
     public void gerarResumo() {
         System.out.println("=== Relatório da Missão ===");
-        System.out.println("Área: " + registro.getMissao().getArea().getId());
-        System.out.println("Drone: " + registro.getMissao().getDrone().getId());
-        System.out.println("Temperatura: " + registro.getTemperatura());
-        System.out.println("Umidade: " + registro.getUmidade());
-        System.out.println("Pragas: " + (registro.isPragas() ? "True" : "False"));
+        System.out.println("[Área] " + registro.getMissao().getArea().getId() + "                 |");
+        System.out.println("[Drone] " + registro.getMissao().getDrone().getId() + "                |");
+        System.out.println("[Temperatura] " + registro.getTemperatura() + "        |");
+        System.out.println("[Umidade] " + registro.getUmidade() + "            |");
+        System.out.println("[Pragas] " + (registro.isPragas() ? "True" : "False") + "            |");
+                System.out.println("===========================");
+
     }
 }
