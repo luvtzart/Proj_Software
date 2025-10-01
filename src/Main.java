1b6aa2f602aae0c6cdbdddfb123b5b2a0aaa5df4
luli <luizamesquita2339@gmail.com>
2025-10-01 08:31:12 -0300

    Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,37 @@
-public class Main{
+public class Main {
     public static void main(String[] args) {
-        // Criando instâncias de Area e Drone
+        
         Area area1 = new Area(1000, "A1", "Fazendinha", "Milho");
+        Area area2 = new Area(2000, "B1", "Sítio Lagos", "Soja");
+        
         Drone drone1 = new Drone("D1", "Disponível");
-
-        // Criando um Agendamento
-        Agendamento agendamento1 = new Agendamento("AG1", "2024-06-01", area1, drone1);
-
-        // Criando um RegistroPlantacao
-        RegistroPlantacao registro1 = new RegistroPlantacao("imagem1.jpg", agendamento1, drone1);
-
-        // Criando um Relatorio
-        Relatorio relatorio1 = new Relatorio(registro1);
-
-        // Gerando o resumo do relatório
-        relatorio1.gerarResumo();
+        Drone drone2 = new Drone("D2", "Disponível");
+        
+        Agendamento ag1 = new Agendamento("AG1", "2024-06-01", area1, drone1);
+        Agendamento ag2 = new Agendamento("AG2", "2024-06-02", area2, drone2);
+        Agendamento ag3 = new Agendamento("AG3", "2024-06-03", area1, drone1);
+        
+        System.out.println("\nVoos agendados: ");
+        ag1.agendarVoo();
+        ag2.agendarVoo();
+        ag3.agendarVoo();
+        
+        RegistroPlantacao registro1 = new RegistroPlantacao("imagem1.jpg", ag1, 25.5, 60.0, false);
+        RegistroPlantacao registro2 = new RegistroPlantacao("imagem2.jpg", ag2, 30.0, 55.0, true);
+        
+        Relatorio rel1 = new Relatorio(registro1);
+        Relatorio rel2 = new Relatorio(registro2);
+        
+        System.out.println("\nRelatorio dos voos ");
+        rel1.gerarResumo();
+        rel2.gerarResumo();
+        
+        System.out.println("\nVoos concluidos: ");
+        ag1.concluirVoo();
+        ag2.concluirVoo();
+        
+        System.out.println("\nAgendar voo:");
+        ag3.agendarVoo(); 
     }
 }
+
